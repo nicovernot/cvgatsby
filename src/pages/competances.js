03851a6eac0b3e0239ca0f91f50fed71c199377c
nicovernot <nicovernot@gmail.com>
2020-08-17 11:16:11 +0000

    comp1

--- src/pages/competances.js
+++ src/pages/competances.js
@@ -2,27 +2,32 @@ import React from 'react'
 import Layout from '../components/layout';
 import CustomQueryStringComponent from "../components/customQueryStringComponent"
 import customQueryStringComponent from '../components/customQueryStringComponent';
+let params = new URLSearchParams(document.location.search.substring(1));
+let param = params.get("cat"); 
 
 export default function competances({data}) {
+
   console.log(data)
-  console.log(customQueryStringComponent)
+ 
     {
     return (
       <Layout>
           <br/>
           <div className="container-fluid">
-          <div className="row">
-                { data.allAirtable.edges.map((node) => (
-                    <div className="card col-md-12" style={{margin: 10}}>
-               <h3>{node.node.data.Name}</h3>
-                    <div className="card-body">
-                        <img src={node.node.data.Attachments[0].url}/>
-                        <p className="card-text" dangerouslySetInnerHTML={{ __html: node.node.data.competances.childMarkdownRemark.html }}></p>
+                { data.allAirtable.edges.map((node,key) => (
+         <div key={key} className="row">
 
+                    <div className="card col-md-12" style={{margin: 10}}>
+                <h3>{key}  --{node.node.data.Name}</h3>
+                    <div className="card-body alert-info">
+                        <img src={node.node.data.Attachments[0].url} style={{width: 180}}/>
+                        <p className="card-text" dangerouslySetInnerHTML={{ __html: node.node.data.competances.childMarkdownRemark.html }}></p>
+                       
+                       <h4><span className="badge badge-info">{node.node.data.cats.join(",")}</span></h4>
                     </div>
                     </div>
-                ))}
            </div>         
+                ))}
            </div>  
       </Layout>
     );
