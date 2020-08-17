74faf4d7367b8efb0a73ca02d5f8bb9879a79719
nicovernot <nicovernot@gmail.com>
2020-08-17 13:56:59 +0000

    filtre competances

--- src/pages/competances.js
+++ src/pages/competances.js
@@ -1,31 +1,41 @@
 import React from 'react'
 import Layout from '../components/layout';
-import CustomQueryStringComponent from "../components/customQueryStringComponent"
-import customQueryStringComponent from '../components/customQueryStringComponent';
-let params = new URLSearchParams(document.location.search.substring(1));
-let param = params.get("cat"); 
+import { Reveal, Tween } from 'react-gsap';
+const params = new URLSearchParams(document.location.search.substring(1));
+const param =params.get("cat"); 
+let boolparam =false
+
 
 export default function competances({data}) {
-
-  console.log(data)
- 
+     
+    let result = data.allAirtable.edges
+    let filterdata = result.filter(function (node) {
+     if(param) return node.node.data.cats[0]===decodeURI(param);
+     return node
+  })
+    console.log(filterdata) 
+    
     {
     return (
       <Layout>
           <br/>
+           { param ? "param":"pas param"}
           <div className="container-fluid">
-                { data.allAirtable.edges.map((node,key) => (
+                { filterdata.map((node,key) => (
          <div key={key} className="row">
-
-                    <div className="card col-md-12" style={{margin: 10}}>
-                <h3>{key}  --{node.node.data.Name}</h3>
-                    <div className="card-body alert-info">
-                        <img src={node.node.data.Attachments[0].url} style={{width: 180}}/>
-                        <p className="card-text" dangerouslySetInnerHTML={{ __html: node.node.data.competances.childMarkdownRemark.html }}></p>
-                       
-                       <h4><span className="badge badge-info">{node.node.data.cats.join(",")}</span></h4>
-                    </div>
-                    </div>
+            <Reveal >
+            <Tween from={{ opacity: 0 }} duration={2}>
+                <div className="card col-md-12" style={{margin: 10}}>
+                            <h3>{key}  --{node.node.data.Name}</h3>
+                                <div className="card-body alert-info">
+                                    <img src={node.node.data.Attachments[0].url} style={{width: 180}}/>
+                                    <p className="card-text" dangerouslySetInnerHTML={{ __html: node.node.data.competances.childMarkdownRemark.html }}></p>
+                                    <h4><span className="badge badge-info">{node.node.data.cats.join(",")}</span></h4>
+                                </div>
+                </div>
+            </Tween>
+            </Reveal>
+                    
            </div>         
                 ))}
            </div>  
@@ -36,7 +46,7 @@ export default function competances({data}) {
 
 export const query = graphql`
   query {
-    allAirtable {
+    allAirtable{
         edges {
         node {
             data {
